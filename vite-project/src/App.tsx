8065683fb6d4c27080daf6d68248b5ea0945f108
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-10-11 18:33:19 +0200

    Try Tailwind works

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,7 @@ import { Accordion } from "./components/accordion/Accordion";
 import { Navbar } from "./components/navbar/Navbar";
 import Calendar from "./components/calendar/Calendar";
 import 'tailwindcss/tailwind.css';
+import './components/calendar/styles.css';
 
 
 export default function Main() {
--- vite-project/src/components/calendar/Calendar.tsx
+++ vite-project/src/components/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import 'tailwindcss/tailwind.css';
+// import 'tailwindcss/tailwind.css';
 import { Menu, Transition } from '@headlessui/react'
 import { DotsVerticalIcon } from '@heroicons/react/outline'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
@@ -63,148 +63,115 @@ const meetings = [
   },
 ]
 
-function classNames(...classes) {
+function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(' ')
 }
 
 function Calendar() {
-  let today = startOfToday()
-  let [selectedDay, setSelectedDay] = useState(today)
-  let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
-  let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
+  const today = startOfToday()
+  const [selectedDay, setSelectedDay] = useState(today)
+  const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
+  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
 
-  let days = eachDayOfInterval({
+  const days = eachDayOfInterval({
     start: firstDayCurrentMonth,
     end: endOfMonth(firstDayCurrentMonth),
   })
 
   function previousMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
+    const firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
   function nextMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+    const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
-  let selectedDayMeetings = meetings.filter((meeting) =>
+  const selectedDayMeetings = meetings.filter((meeting) =>
     isSameDay(parseISO(meeting.startDatetime), selectedDay)
   )
 
   return (
-    <html>
-      <head>
-        <link href="/dist/output.css" rel="stylesheet" />
-        <link rel="stylesheet" href="./styles.css" />
-      </head>
-    <div className="pt-16">
-      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
-        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-          <div className="md:pr-14">
-            <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-900">
-                {format(firstDayCurrentMonth, 'MMMM yyyy')}
-              </h2>
-              <button
-                type="button"
-                onClick={previousMonth}
-                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Previous month</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-              <button
-                onClick={nextMonth}
-                type="button"
-                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Next month</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
-              <div>S</div>
-              <div>M</div>
-              <div>T</div>
-              <div>W</div>
-              <div>T</div>
-              <div>F</div>
-              <div>S</div>
-            </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
-              {days.map((day, dayIdx) => (
-                <div
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day)],
-                    'py-1.5'
-                  )}
-                >
-                  <button
-                    type="button"
-                    onClick={() => setSelectedDay(day)}
-                    className={classNames(
-                      isEqual(day, selectedDay) && 'text-white',
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        'text-red-500',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-900',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-400',
-                      isEqual(day, selectedDay) && isToday(day) && 'bg-red-500',
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        'bg-gray-900',
-                      !isEqual(day, selectedDay) && 'hover:bg-gray-200',
-                      (isEqual(day, selectedDay) || isToday(day)) &&
-                        'font-semibold',
-                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
-                    )}
-                  >
-                    <time dateTime={format(day, 'yyyy-MM-dd')}>
-                      {format(day, 'd')}
-                    </time>
-                  </button>
-
-                  <div className="w-1 h-1 mx-auto mt-1">
-                    {meetings.some((meeting) =>
-                      isSameDay(parseISO(meeting.startDatetime), day)
-                    ) && (
-                      <div className="w-1 h-1 rounded-full bg-sky-500"></div>
-                    )}
+    <>
+      <html>
+        <head>
+          <link href="/dist/output.css" rel="stylesheet" />
+          <link rel="stylesheet" href="./styles.css" />
+        </head>
+        <body>
+          <div className="pt-16">
+            <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+              <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+                <div className="md:pr-14">
+                  <div className="flex items-center">
+                    <h2 className="flex-auto font-semibold text-gray-900">
+                      {format(firstDayCurrentMonth, 'MMMM yyyy')}
+                    </h2>
+                    <button
+                      type="button"
+                      onClick={previousMonth}
+                      className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">Previous month</span>
+                      <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                    </button>
+                    <button
+                      onClick={nextMonth}
+                      type="button"
+                      className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">Next month</span>
+                      <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>T</div>
+                    <div>W</div>
+                    <div>T</div>
+                    <div>F</div>
+                    <div>S</div>
+                  </div>
+                  <div className="grid grid-cols-7 mt-2 text-sm">
+                    {days.map((day, dayIdx) => (
+                      <div
+                        key={day.toString()}
+                        className={classNames(
+                          dayIdx === 0 && colStartClasses[getDay(day)] || '',
+                          'py-1.5'
+                        )}
+                      >
+                        {format(day, 'd')}
+                      </div>
+                    ))}
                   </div>
                 </div>
-              ))}
+                <section className="mt-12 md:mt-0 md:pl-14">
+                  <h2 className="font-semibold text-gray-900">
+                    Schedule for{' '}
+                    <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>
+                      {format(selectedDay, 'MMM dd, yyy')}
+                    </time>
+                  </h2>
+                  <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+                    {selectedDayMeetings.length > 0 ? (
+                      selectedDayMeetings.map((meeting) => (
+                        <Meeting meeting={meeting} key={meeting.id} />
+                      ))
+                    ) : (
+                      <p>No meetings for today.</p>
+                    )}
+                  </ol>
+                </section>
+              </div>
             </div>
           </div>
-          <section className="mt-12 md:mt-0 md:pl-14">
-            <h2 className="font-semibold text-gray-900">
-              Schedule for{' '}
-              <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>
-                {format(selectedDay, 'MMM dd, yyy')}
-              </time>
-            </h2>
-            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
-              {selectedDayMeetings.length > 0 ? (
-                selectedDayMeetings.map((meeting) => (
-                  <Meeting meeting={meeting} key={meeting.id} />
-                ))
-              ) : (
-                <p>No meetings for today.</p>
-              )}
-            </ol>
-          </section>
-        </div>
-      </div>
-    </div>
-    </html>
-  )
+        </body>
+      </html>
+    </>
+  );
 }
 
 function Meeting({ meeting }) {
@@ -286,7 +253,7 @@ function Meeting({ meeting }) {
   )
 }
 
-let colStartClasses = [
+const colStartClasses = [
   '',
   'col-start-2',
   'col-start-3',
