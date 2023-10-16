1376a9efcf3979c0352f63fecd57210b1ddc71fd
Jarvis <94354305+Jarvis-69@users.noreply.github.com>
2023-10-16 20:58:01 +0200

    footer add

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,7 @@
 // import { Navbar } from "./components/navbar/Navbar";
 import { Calendar } from "./components/calendar/Calendar";
 import Card from "./components/card/Card";
+import Footer from "./components/footer/Footer";
 
 export default function Main() {
   return (
@@ -10,6 +11,7 @@ export default function Main() {
       {/* <Accordion /> */}
       <Calendar />
       <Card />
+      <Footer />
     </main>
   );
 }
